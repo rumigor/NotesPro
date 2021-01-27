0fbe1f638435c187aa02253683a9dfbf66b6fb79
rumigor <rumigor@gmail.com>
2021-01-27 09:38:00 +0300

    Initial commit

---
+++ app/src/main/java/com/lenecoproekt/notespro/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.lenecoproekt.notespro.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.lenecoproekt.notespro.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
